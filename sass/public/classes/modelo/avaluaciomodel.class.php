475dc6582ca0b1b3e829735f18e7544ddc9d74ba
bisbe00 <bisbe00@gmail.com>
2024-04-17 21:31:26 +0200

    hola, he cumnplido documento model update y delete

--- sass/public/classes/modelo/avaluaciomodel.class.php
+++ sass/public/classes/modelo/avaluaciomodel.class.php
@@ -19,4 +19,3 @@ class AvalucaioModel implements CRUDable
 
     }
 }
-?>
--- sass/public/classes/modelo/documentmodel.class.php
+++ sass/public/classes/modelo/documentmodel.class.php
@@ -57,11 +57,44 @@ class DocumentModel
         }
     }
 
-    public function update($obj)
+    public function update(Document $obj)
     {
+        if (count($this->read($obj)) !== 0) {
+            $query = "UPDATE document SET nom = :nom, tipus = :tipus, link = :link, proces_id = :proces_id WHERE id = :id";
+            $statement = $this->pdo->prepare($query);
+
+            $nom = $obj->__get('nom');
+            $tipus = $obj->__get('tipus');
+            $link = $obj->__get('link');
+            $proces_id = $obj->__get('proces_id');
+            $id = $obj->__get('id');
+
+            $statement->bindParam(':nom', $nom, PDO::PARAM_STR);
+            $statement->bindParam(':tipus', $tipus, PDO::PARAM_STR);
+            $statement->bindParam(':link', $link, PDO::PARAM_STR);
+            $statement->bindParam(':proces_id', $proces_id, PDO::PARAM_INT);
+            $statement->bindParam(':id', $id, PDO::PARAM_INT);
+
+            $state = $statement->execute();
+            if ($state) {
+                $statement->closeCursor();
+                return true;
+            }
+        }
+        return false;
     }
 
-    public function delete($obj)
+    public function delete(Document $obj)
     {
+        if (count($this->read($obj)) !== 0) {
+            $query = "DELETE FROM document WHERE id = ?";
+            $statement = $this->pdo->prepare($query);
+            $state = $statement->execute([$obj->__get('id')]);
+            if ($state) {
+                $statement->closeCursor();
+                return true;
+            }
+        }
+        return false;
     }
 }
